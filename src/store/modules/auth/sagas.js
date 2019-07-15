574a8a4fe847ee3ae8363a6aa8fb073e6fe30e39
dionlopes <dion.mello@hotmail.com>
2019-07-15 19:29:26 -0300

    feature: Cadastro na aplicação

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -15,8 +15,6 @@ export function* signIn({ payload }) {
       password,
     });
 
-    console.tron.log(response.data);
-
     const { token, user } = response.data;
 
     if (!user.provider) {
@@ -33,4 +31,26 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (error) {
+    toast.error('Falha no cadastro, verifique seus dados!');
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
